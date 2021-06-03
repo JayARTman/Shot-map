e662688b4e2cc4d9f9199296f818499bef8d42a3
P Stenger <pstenger23@gmail.com>
2021-06-03 11:57:16 -0500

    server set up

---
+++ server.js
@@ -0,0 +1,15 @@
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3008
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listenging on ${PORT}`)
+    })
+});
