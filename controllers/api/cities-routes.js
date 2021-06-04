d68d5cd7e12a8ada365bcaca2a7369ef73881662
jermz678 <jermz678@live.com>
2021-06-03 21:06:14 -0500

    saving local changes

--- controllers/api/cities-routes.js
+++ controllers/api/cities-routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-
+const { Posts, Users, Cities} = require('../../models')
 
 //user choosing a city to see posts on that city
+
+router.post('./city', (req, res) => {
+    Cities.findAll({
+        
+    })
+})
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -10,25 +10,9 @@ Users.hasMany(Posts, {
     foreignKey: 'user_name'
 });
 
-<<<<<<< HEAD
-// Posts.hasOne(Cities);
-Posts.belongsTo(Cities, {
-    foreignKey: 'location'
-});
-
-Cities.hasMany(Posts, {
-    foreignKey: 'location'
-})
-
-
-
-//     foreignKey: 'city_name'
-// });
-=======
 Cities.belongsTo(Posts, {
     foreignKey: 'city_name'
 });
->>>>>>> c7744365fa55c4bd60a73634eb448468dbf1dad0
 
 Posts.hasOne(Cities);
 
