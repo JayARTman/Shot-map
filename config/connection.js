e1a0eff628bed017e41a3ced504a42cbb69c979d
jermz678 <jermz678@live.com>
2021-06-03 19:35:44 -0500

    yay

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('shot_map', 'root', '2501Estratford!', {
+const sequelize = new Sequelize('shot_map', 'root', 'Valandme123!', {
     host: 'localhost',
     dialect: 'mysql'
 });
@@ -15,6 +15,6 @@ module.exports = sequelize;
 //     },
 //   });
 
-// module.exports = sequelize
+ module.exports = sequelize
 
 
