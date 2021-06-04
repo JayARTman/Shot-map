fd9eb4397e76b96e3b3704eb2ffe7213121acdb1
P Stenger <pstenger23@gmail.com>
2021-06-03 19:51:52 -0500

    miscellaneous errors

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,7 @@ const Sequelize = require('sequelize');
 //     host: 'localhost',
 //     dialect: 'mysql'
 // });
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize('shot_map', process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
