dad3755bdb201831e56f58154d805450a580cd3e
P Stenger <pstenger23@gmail.com>
2021-06-04 22:42:15 -0500

    password verification route file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const postRoutes = require('./post-routes');
 const cityRoutes = require('./cities-routes');
+const passwordVerify = require('./password-verification')
 
 router.use('./post', postRoutes);
 router.use('./city', cityRoutes);
+router.use('./password-verification', passwordVerify)
 
 module.exports = router;
---
+++ controllers/api/password-verification.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { Users } = require('../../models')
+
+module.exports = router;
