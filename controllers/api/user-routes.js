1466dabc6330777b1a4f8fda4918b29d7acfb9b3
P Stenger <pstenger23@gmail.com>
2021-06-04 22:46:10 -0500

    sude code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Posts, Users, Cities} = require('../../models');
 
+//get all users?
 
+//post user/set password
+
+//put (edit) username/password?
+
+//delete user
 
 
