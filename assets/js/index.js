1cd34df3a947c4f310878308994ccd4237229548
jasonmueller <jmuel9999@gmail.com>
2021-06-07 15:43:34 -0500

    Branch updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,11 @@ const searchBtn = document.getElementById('searchBtn');
 
 
 const showSearch = function() {
-    console.log('button works');
-    var city = 'http://localhost:3008/api/post';
+    //console.log('button works');
+    let city = 'http://localhost:3008/api/post';
     fetch(city)
-    .then((response) => console.log(response.json))
-    .then((data) => {
+        .then((response) => console.log(response.json))
+        .then((data) => {
         console.log(data)
     })
 }
