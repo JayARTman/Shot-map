eaa8a7e410c893dd6708af2e13db1bc4524ad189
jermz678 <jermz678@live.com>
2021-06-07 11:19:12 -0500

    starting fetch function

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+//creating fetch request based on city choice
+const cityChoice = document.getElementById('inputGroupSelect04');
+const searchBtn = document.getElementById('searchBtn');
+
+
+
+const showSearch = function() {
+    console.log('button works');
+    var city = 'http://localhost:3008/api/post';
+    fetch(city)
+    .then((response) => console.log(response.json))
+    .then((data) => {
+        console.log(data)
+    })
+}
+
+searchBtn.addEventListener('click', showSearch)
