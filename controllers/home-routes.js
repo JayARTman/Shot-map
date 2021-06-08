bd4b80aa7cc717cf905e53fa0256a897feff31af
P Stenger <pstenger23@gmail.com>
2021-06-08 13:51:22 -0500

    handlebars render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,23 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('about.handlebars');
+    res.render('index');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contactus');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login-form');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
 });
 
 
