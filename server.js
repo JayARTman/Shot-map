a9e3cee4399f5b04a27e14c45aa2f2c1b3ce870a
jasonmueller <jmuel9999@gmail.com>
2021-06-08 16:37:23 -0500

    Merge with develop

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'../..', 'assets')));
+app.use(express.static(path.join(__dirname, '/assets')));
 
 app.use(routes);
 
