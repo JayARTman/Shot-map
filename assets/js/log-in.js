30bdbd07fbad3df122afa0ac19c744142c895cf3
jasonmueller <jmuel9999@gmail.com>
2021-06-09 11:02:19 -0500

    Log in button functionality works and displays the value of username and password.

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -1,9 +1,15 @@
-const passwordVerify = require('../../controller/api/password-verification.js');
+// const passwordVerify = require('../../controller/api/password-verification.js');
 const logInBtn = document.getElementById('logInBtn');
 
 // get user info function for user sign up
 const getUserLogin = function() {
-    console.log('getUserLogin works')
+    let username = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    //console.log('function works');
+    console.log('Username is ' + username + 'and your password is ' + password);
 }
 
-logInBtn.addEventListener('click', getUserLogin());
+logInBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    getUserLogin();
+});
