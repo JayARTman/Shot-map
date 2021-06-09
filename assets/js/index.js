30cdba044d50b069ac462b624c8b5499e77a8cf5
jermz678 <jermz678@live.com>
2021-06-08 19:32:33 -0500

    fixing git pull

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+//creating fetch request based on city choice
+const cityChoice = document.getElementById('inputGroupSelect04');
+const searchBtn = document.getElementById('searchBtn');
+const signUpBtn = document.getElementById('signUpBtn');
+
+// const showSearch = function() {
+//   //console.log('button works');
+//     let city = 'http://localhost:3008/api/post';
+//     fetch(city)
+//         .then((response) => console.log(response.json))
+//         .then((data) => {
+//         console.log(data)
+//     })
+// }
+
+// get user info function for user sign up
+<<<<<<< HEAD:public/js/index.js
+const getUserInfo = function() {
+    console.log('getUserInfo works')
+}
+
+//signUpBtn.addEventListener('click', getUserInfo)
+searchBtn.addEventListener('click', showSearch)
+=======
+// const getUserInfo = function() {
+//     console.log('getUserInfo works')
+// }
+// getUserInfo();
+// signUpBtn.addEventListener('click', getUserInfo())
+// searchBtn.addEventListener('click', showSearch)
+>>>>>>> 625ba82e8a6757939e849af63933045a5b1d4550:assets/js/index.js
