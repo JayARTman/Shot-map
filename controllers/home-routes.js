4c492f9dda43571f25653ba5d2a5594e461767ec
jermz678 <jermz678@live.com>
2021-06-08 19:32:33 -0500

    fixing git pull

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,20 +6,27 @@ const { Posts, Users, Cities} = require('../models')
 //     res.render('index');
 // });
 
-router.get('/', (req, res) => {
-    Posts.findAll({
-      include: [{ model: Users, attributes: ['user_name']},
-      { model: Cities, attributes: ['city_name']}]
-    })
-    .then(dbPostData => res.json(dbPostData));
-    
-    res.render('index')
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contactus');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login-form');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
 
     // .catch(err => {
     //   console.log(err);
     //   res.status(500).json(err);
     // });
-})
+
 // const sequelize = require('../config/connection');
 
 // router.get('/', (req, res) => {
