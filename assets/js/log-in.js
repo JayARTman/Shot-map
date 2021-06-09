23f8a4554fe58f19490e12a2b3c3c2e8a83b9cbf
jasonmueller <jmuel9999@gmail.com>
2021-06-09 17:25:28 -0500

    develop merge, no noteable changes

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -5,8 +5,14 @@ const logInBtn = document.getElementById('logInBtn');
 const getUserLogin = function() {
     let username = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    //console.log('function works');
-    console.log('Username is ' + username + 'and your password is ' + password);
+    //console.log('Username is ' + username + ' and your password is ' + password);
+
+    fetch('/:user_name')
+        .then(res => res.json())
+        console.log(getUserLogin)
+
+
+    //for (i = 0; )
 }
 
 logInBtn.addEventListener('click', (event) => {
