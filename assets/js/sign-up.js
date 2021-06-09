2f169ff9c053716ed468d39da6b3469fbc0e8fac
jermz678 <jermz678@live.com>
2021-06-08 20:17:15 -0500

    testing sign up

---
+++ assets/js/sign-up.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById('btn');
+
+      const submitForm = function() {
+        const fname = document.getElementById('fname').value;
+        
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        
+        fetch('/api/user', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            'user_name': fname,
+            'email': email,
+            'password': password
+          })
+        })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data)
+        })
+
+        
+      }
+      btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        submitForm();
+      })
