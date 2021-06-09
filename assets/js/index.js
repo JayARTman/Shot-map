607814d2ce1dde4a67d23e70bc73c809b8a285c9
P Stenger <pstenger23@gmail.com>
2021-06-09 17:05:14 -0500

    trying handlebars

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,17 @@
 
+const what = ['1', 12, 'ok'];
+// const displayPosts = function() {
+
+
+    
+
+//     console.log(postsToGo.length);
+//     console.log(what)
+    
+    
+// }
 const getPosts = function() {
-    const content = document.getElementById('content');
-    const postsData = [];
+    const postToGo = [];
     fetch('/api/post', {
         method: 'GET',
         headers: { 'Content-Type': 'application/json'}
@@ -10,12 +20,34 @@ const getPosts = function() {
         return res.json();
     })
     .then(data => {
-        for(i=0; i <= data.length-1; i++) {
-            postsData.push(data[i]);
+
+        
+
+        for(i=0; i <= data.length - 1; i++) {
+            const context = 
+                {
+                    'id': data[i].id,
+                    'username': data[i].user.user_name,
+                    'city': data[i].city.city_name
+                }
+            postToGo.push(context);
+            
+            
         }
+        
+        // displayPosts(postsData);
     })
-    return postsData;
-    
+    .then(() => {
+        const script = document.getElementById('posts-template').innerHTML;
+        const template = Handlebars.compile(script);
+        const postInfo = postToGo;
+        const compiledPost = template(postInfo);
+        
+
+        document.getElementById('content').innerHTML = compiledPost;
+        
+    })
+   
 }
 
 getPosts();
