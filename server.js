15669da0bc6a736bf8668bb60deb76b9189dc0db
jermz678 <jermz678@live.com>
2021-06-08 19:25:11 -0500

    changes to file structure

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3008
 
 const sequelize = require('./config/connection');
 
-const routes = require('./controllers');
+//const routes = require('./controllers');
 
 const hbs = exphbs.create({});
 
@@ -17,9 +17,9 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
