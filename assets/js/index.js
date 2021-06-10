fbda9556102f790d629d8bdd53c7da3441dc843e
P Stenger <pstenger23@gmail.com>
2021-06-09 21:23:49 -0500

    post from database

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,44 @@
-
+const fetchData = function() {
+    const url = '/api/post'
+    fetch(url, {
+        method: 'GET',
+    })
+    .then(res => res.json())
+    .then(data => {
+        const script = document.getElementById('posts-template').innerHTML;
+        const template = Handlebars.compile(`<div class="border">
+                         
+        {{#each data}}
+        
+            <div class='border'>              
+                <div>
+                    <p>UserName{{this.user.user_name}}</p>
+            
+                </div>
+                <div>
+            
+                    <img src="../assets/images/150.png">
+                </div>
+                <div>
+                    <p>City Name{{this.city.city_name}}</p>
+                </div>
+                <div>
+                    <p>Description of post: {{ this.title }}</p>
+                </div>
+            </div>
+        
+        {{/each}}
+        `);
+        const context = { data }
+        const compiled = template(context);
+        console.log(context)
+        
+        document.getElementById('content').innerHTML = compiled;
+        })
+}
+const fill = function() {
+    
+    
+}
+fetchData()
 
