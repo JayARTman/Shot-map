ab24ff0a498e53d2a96c5a5c072f0b41db620c76
jasonmueller <jmuel9999@gmail.com>
2021-06-10 13:45:00 -0500

    Merge commit

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -7,12 +7,25 @@ const getUserLogin = function() {
     let password = document.getElementById('password').value;
     //console.log('Username is ' + username + ' and your password is ' + password);
 
-    fetch('/:user_name')
-        .then(res => res.json())
-        console.log(getUserLogin)
+    fetch('/:user_name', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          'user_name': username,
+          'password': password
+        })
+      })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if(!data.id) {
+          alert(data.errors[0].message);
+        }
+        console.log(data);
+        
+      });
 
-
-    //for (i = 0; )
 }
 
 logInBtn.addEventListener('click', (event) => {
