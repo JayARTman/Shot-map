9c5fa3f42a66e97bfc2087f303cba7927fb7f0ab
jermz678 <jermz678@live.com>
2021-06-12 21:06:12 -0500

    nothing really

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,7 @@
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize('shot_map', 'root', '2501Estratford!', {
-//     host: 'localhost',
-//     dialect: 'mysql'
-// });
-// const sequelize = new Sequelize('shot_map', 'root', '2501Estratford!', {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
-
-
-const sequelize = new Sequelize('shot_map', 'root', '2501Estratford!', {
+const sequelize = new Sequelize('shot_map', 'root', 'Valandme123!', {
      host: 'localhost',
     dialect: 'mysql'
 });
@@ -30,14 +16,6 @@ const sequelize = new Sequelize('shot_map', 'root', '2501Estratford!', {
 //     });
 
 module.exports = sequelize;
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     dialectOptions: {
-//       decimalNumbers: true,
-//     },
-//   });
 
-//module.exports = sequelize;
 
 
