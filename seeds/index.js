dc90dee28e847fe2e395b4c8316b23c95079e50d
P Stenger <pstenger23@gmail.com>
2021-06-15 15:02:50 -0500

    push for seeds

--- seeds/index.js
+++ seeds/index.js
@@ -4,8 +4,10 @@ const sequelize = require('../config/connection');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
+
     await seedCities();
-    console.log('\n----- CATEGORIES SEEDED -----\n')
-};
+
+    process.exit(0);
+}
 
 seedAll();
