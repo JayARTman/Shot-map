2cc635b8c9945b081a335ca4b65017c40de1179a
jasonmueller <jmuel9999@gmail.com>
2021-06-15 09:47:47 -0500

    sequel password.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('shot_map', 'root', 'Valandme123!', {
+const sequelize = new Sequelize('shot_map', 'root', 'Superimportantsequel1423!', {
      host: 'localhost',
     dialect: 'mysql'
 });
