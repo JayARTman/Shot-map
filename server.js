03a4ec9f0a52e826b759062de3af63e5d0a0338f
P Stenger <pstenger23@gmail.com>
2021-07-09 11:34:45 -0500

    push to heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3008
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Secret cookies',
